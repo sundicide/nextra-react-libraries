01a6ad50f5eb8877842ed340cc746f4bc5a2440f
sundicide <kyusuen@gmail.com>
2023-02-14 16:54:54 +0900

    update log title

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
-  project: {
-    link: 'https://github.com/sundicide/nextra-react-libraries',
-  },
-  // ...
-}
+	logo: <span>sundicide Nextra Documentation</span>,
+	project: {
+		link: "https://github.com/sundicide/nextra-react-libraries",
+	},
+	// ...
+};
